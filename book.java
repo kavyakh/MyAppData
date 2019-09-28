042ee806101b0f1fdf78acfcbe862a2a4f0f9b24
kavya kh <kavyakh12@gmail.com>
2019-09-28 15:21:53 -0700

    added1.

---
+++ book.java
@@ -0,0 +1,18 @@
+public class book{
+
+private bookId;
+private bookname;
+
+public int getbookId() {
+        return this.bookId;
+    }
+ 
+    public void setbookId(int bookId) {
+        this.bookId = bookId;
+
+public String getbookname(){
+     return this.bookname
+}
+public void setbookName(String bookname){
+     this.bookname = bookname	
+}
